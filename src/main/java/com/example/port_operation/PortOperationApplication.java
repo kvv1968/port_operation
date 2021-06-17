86c2ab0872fa6b7b2f7bb7acfeb8cd4e1d87de78
Vadim Karpov <ksvkvv1990@gmail.com>
2021-06-17 19:54:06 +0300

    feature/0001 Создание проекта Порт на Spring Boot

---
+++ src/main/java/com/example/port_operation/PortOperationApplication.java
@@ -0,0 +1,13 @@
+package com.example.port_operation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortOperationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortOperationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/port_operation/PortOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.port_operation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortOperationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
