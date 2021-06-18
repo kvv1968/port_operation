5498d56c03aec905e012c7168033f9b652530433
Vadim Karpov <ksvkvv1990@gmail.com>
2021-06-19 00:21:57 +0300

    feature/6

--- src/main/java/com/example/port_operation/service/implemen/RaidServiceImpl.java
+++ src/main/java/com/example/port_operation/service/implemen/RaidServiceImpl.java
@@ -3,11 +3,12 @@ package com.example.port_operation.service.implemen;
 import com.example.port_operation.model.Raid;
 import com.example.port_operation.model.Ship;
 import com.example.port_operation.service.interfaces.RaidService;
+import com.sun.istack.NotNull;
 
 public class RaidServiceImpl implements RaidService {
 
     @Override
-    public boolean isFreeRaid(int capacity) {
+    public boolean isFreeRaid(@NotNull int capacity) {
         Raid raid = Raid.getInstance(capacity);
         int size = raid.getShipList().size();
         return size < capacity;
